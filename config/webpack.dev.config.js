68d3d6206d75cb99d6bda89542a13cf09d6e81a7
xia.huang <xia.huang@convertlab.com>
2022-05-21 11:16:55 +0800

    feat: 配置文件增加注释

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -8,7 +8,7 @@ module.exports = Object.assign({}, baseConfig, {
     // 处理mock跨域问题，访问mock接口方式改为：http://localhost:3000/api/..
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "http://localhost:3001", // target是请求实际目标服务器地址，这里是mock服务地址和端口。前端访问通过访问3000，proxy会自动代理到3001
         secure: false,
       },
     },
